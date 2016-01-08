1a2f4f5fe8fec5b2f76989b7973a0547ff5ab323
maumasi <liu.maumasi@gmail.com>
2016-01-08 00:05:52 -0500

    working and updated code

--- library/library.py
+++ library/library.py
@@ -1,62 +1,111 @@
+# Import random function and methods
 import random
 
+# Library() class used to make all calculations in Newton's 3rd Law experiment in main.py
 class Library():
     def __init__(self):
-        print "Library created"
-        self.mass = 0 #
-        self.acceleration = 0 #
-        self.your_force = 0 #
-        self.obj_force = 0 #
-        self.your_area = 0 #
-        self.obj_area = 0 #
+        self.mass = 0
+        self.acceleration = 0
+        self.your_force = 0
+        self.obj_force = 0
+        self.your_area = 0
+        self.obj_area = 0
         self.your_pressure = 0
         self.obj_pressure = 0
-        self.winner = True
+        self.pressure_dif = 0
+        self.obj_vs = ""
+        self.broke_through = True
 
-
-
+# force method used for generating your_force value after user input
     def force(self):
         self.your_force = int(self.acceleration) * int(self.mass)
-        return self.your_force
 
+
+# pascals_principle method used for:
+#     -generating obj name you're going up against
+#     -generating obj_area
+#     -generating obj_force
+#     -generating obj_pressure
+#     -generating your_pressure
+#
+# All of these except the obj's name are used to calculate
+# the accurate pressure for you and the obj you're going up against
     def pascals_principle(self):
 
 # the value of the keys are used for Library.obj_area to get
 # a value for an object's area to calculate the object's force.
-        obj_dic = {"Piano": 6, "Brick wall": 10, "Paper poster": 2, "Stack of milk bottles": 3}
+        obj_dic = {"Piano": 50, "Brick wall": 100, "Glass window": 2, "Stack of milk bottles": 3, "Panzer Tank": 1000}
+
+# Turn all obj_dic's keys into elements in a list
         obj_list = obj_dic.keys()
         index = obj_list[random.randrange(0, len(obj_list))]
 
+        self.obj_vs = index
         self.obj_area = obj_dic[index]
         self.obj_force = (int(self.your_force) * int(self.obj_area)) / int(self.your_area)
-        return self.obj_force
 
+        self.obj_pressure = self.obj_force / int(self.your_area)
+        self.your_pressure = int(self.your_force) / int(self.obj_area)
+
+# winner method used to test if broke_through should be True or False
     def winner(self):
-        if self.obj_force < self.your_force:
-            self.winner = True
-        else:
-            self.winner = False
 
+        if self.obj_pressure < self.your_pressure:
+            self.pressure_dif = self.your_pressure - self.obj_pressure
+            self.broke_through = True
+        else:
+            self.pressure_dif = self.obj_pressure - self.your_pressure
+            self.broke_through = False
         return self.winner
 
 
-
+# Printer() class used to print out messages based on results from user inputs
 class Printer():
     def __init__(self):
+        print "Printer created"
 
-        def greeting():
-            print "\nLet's see what you can run through? \nSound painful, It might be. Let's play!"
+# greeting method used to explain what this app is attempting to do.
+    def greeting(self):
+        print "\nLet's put Newton's 3rd Law to the test. " \
+              "\nDo objects exert equal and opposite forces when in contact with each other?" \
+              "\nSound painful, It might be. Let's play!\n"
+        return
 
-        def outcome(bool, ):
-            if bool:
-                print "\nYou survived! You broke through!"
-            else:
-                print "\nDang, looks like you need to go to the hospital."
+# outcome method used to print out the outcome message to the user baes on their results
+    def outcome(self,
+                bool,
+                obj_description,
+                pressure_dif,
+                obj_pressure,
+                your_pressure):
+# tie outcome
+        if obj_pressure == your_pressure:
+            print "\nOUTCOME:" \
+                  "You almost made it through, " \
+                  "\nbut it looks like you and the" + obj_description + " " \
+                  "\nhad the same amount of pressure pushing back on each other."
+# user win outcome
+        elif bool:
+            print "\nOUTCOME:" \
+                  "\nYou broke through a " + obj_description + "! " \
+                  "\nYou might need medical attention, but you did it!! " \
+                  "\nYou had " + str(pressure_dif) + "Kg per sq/meter than the " + obj_description + "."
+# user lose outcome
+        else:
+            print "\nOUTCOME:" \
+                  "\nDang, looks like you need to go to the hospital. " \
+                  "\nYou just ran into a " + obj_description + " as hard as you could, your a dumb, dummy. " \
+                  "\nThe " + obj_description + " had " + str(pressure_dif) + "Kg per sq/meter more than you."
 
-
-
-
-
-
-
-
+# summery method is used to print out a report of why the user mon or lost
+    def summery(self,
+                obj_description,
+                obj_pressure,
+                obj_force,
+                your_force,
+                your_pressure):
+        print "\nSUMMERY:" \
+              "\nYour Force: " + str(your_force) + " " + \
+              "\n" + obj_description + "'s Force: " + str(obj_force) + " " \
+              "\nYour total pressure: " + str(your_pressure) + " " + \
+              "\n" + obj_description + "'s total pressure: " + str(obj_pressure)
--- library/main.py
+++ library/main.py
@@ -1,20 +1,23 @@
-
+# Import the whole libaray.py file
 from library import *
 
 class MainHandler():
     def __init__(self):
 
+# instances of the classes: Library & Printer
         calc_results = Library()
         narrator = Printer()
 
-# I used try/except to test if an integer was entered. If not then the user will be prompted again
+# An introdution to what this app is about
+        narrator.greeting()
+
+# I used try/except to test if an integer was entered. If not then the user will be prompted again.
+# the bool before the while loop is used to keep the loop active until the try condition is met.
         mass_is_int = True
         while mass_is_int:
             try:
                 calc_results.mass = raw_input("Enter weight:\n")
                 isinstance(int(calc_results.mass), int)
-
-                print calc_results.mass
 
                 mass_is_int = False
             except Exception:
@@ -23,50 +26,52 @@ class MainHandler():
 
                 mass_is_int = True
 
-# I used try/except to test if an integer was entered. If not then the user will be prompted again
+# I used try/except to test if an integer was entered. If not then the user will be prompted again.
+# the bool before the while loop is used to keep the loop active until the try condition is met.
         accel_is_int = True
         while accel_is_int:
             try:
-                calc_results.acceleration = raw_input("Enter your max running speed in meters (in integers):\n")
+                calc_results.acceleration = raw_input("Enter your max running speed in meters/second (in integers):\n")
                 isinstance(int(calc_results.acceleration), int)
-
-                print calc_results.acceleration
 
                 accel_is_int = False
             except Exception:
 
                 print "You didn't enter a number!"
-
                 accel_is_int = True
 
-# I used try/except to test if an integer was entered. If not then the user will be prompted again
+# I used try/except to test if an integer was entered. If not then the user will be prompted again.
+# the bool before the while loop is used to keep the loop active until the try condition is met.
         area_is_int = True
         while area_is_int:
             try:
-                calc_results.your_area = raw_input("Enter your body's front surface area in square meters \n(as close as posible using integers)\n")
+                calc_results.your_area = raw_input("Enter your body's front surface area in square meters "
+                                                   "\n(as close as posible using integers)\n")
                 isinstance(int(calc_results.your_area), int)
-
-                print calc_results.your_area
-
                 area_is_int = False
             except Exception:
                 print "You didn't enter a number!"
                 area_is_int = True
 
+# Run all calulations after getting all the needed info
+        calc_results.force()
+        calc_results.pascals_principle()
+        calc_results.winner()
 
-        if calc_results.winner:
-            event_outcome.
-            pass
+# print out the outcome of the collision of the player and the object
+        narrator.outcome(calc_results.broke_through,  # ...boolean fore breaking through obj
+                         calc_results.obj_vs,  # ..........The obj name you ran up against
+                         calc_results.pressure_dif,  # ....pressure difference bwteen you and obj
+                         calc_results.obj_pressure,  # ....obj's total pressure
+                         calc_results.your_pressure)  # ...your total pressure
 
+        narrator.summery(calc_results.obj_vs,  # .........The obj name you ran up against
+                         calc_results.obj_pressure,  # ...obj's total pressure
+                         calc_results.obj_force,  # ......obj's total fore
+                         calc_results.your_force,  # .....your total fore
+                         calc_results.your_pressure)  # ..your total pressure
 
-        # print play.force()
-        #
-        # print
-        # print play.obj_force
-        # print play.your_force
-        #
-        # print play.pascals_principle()
-
+# Instantiate MainHandler() class
 play_it = MainHandler()
 
 
